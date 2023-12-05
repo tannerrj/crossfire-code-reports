20858b33f7019d975759feccf1c5efbed198a633
Rick Tanner <leaf@real-time.com>
2023-12-05 01:29:42 -0600

    Add Project URL to the gitlog_summary.sh header

--- scripts/gitlog_summary.sh
+++ scripts/gitlog_summary.sh
@@ -3,6 +3,7 @@
 # Script: gitlog_summary.sh
 # Author: Rick Tanner via ChatGPT
 # Description: This script runs `git-log-html` for various Crossfire code repositories, saves the output to the website directory, and provides a summary of the execution time.
+# Project URL: https://github.com/tannerrj/crossfire-code-reports
 
 # Step 1: Function to handle errors
 handle_error() {
